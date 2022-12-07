b51cd44d3a89b8e9338b4681ed0b73eff760aa27
Roshan Patel <ovicior@gmail.com>
2022-12-07 18:03:09 -0500

    added appointments route with html template

--- flask-app/src/views.py
+++ flask-app/src/views.py
@@ -13,3 +13,32 @@ def home():
 @views.route('/test')
 def tester():
     return "<h1>this is a test!</h1>"
+
+@views.route('/monkey')
+def monkey():
+    return "<h1>this is a monkey!</h1>"
+
+@views.route('/appointments')
+def get_appointments():
+    return """<table>
+  <tr>
+    <th>Time Slot</th>
+    <th>Service</th>
+    <th>Employee</th>
+  </tr>
+  <tr>
+    <td>9/1/22 10:30 AM</td>
+    <td>Color</td>
+    <td>Katie Li</td>
+  </tr>
+  <tr>
+    <td>9/3/22 12:30 PM</td>
+    <td>Color</td>
+    <td>Mary Smith</td>
+  </tr>
+    <tr>
+    <td>9/7/22 11:30 AM</td>
+        <td>Cut</td>
+        <td>Rodrick Heffley</td>
+ </tr>
+</table>"""
